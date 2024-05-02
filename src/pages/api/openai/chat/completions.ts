46594c2a64759d8e6499100f03a8f9e6f6f8f4ca
arelice <141432992+arelice@users.noreply.github.com>
2024-05-02 17:38:11 +0800

    Update completions.ts

--- src/pages/api/openai/chat/completions.ts
+++ src/pages/api/openai/chat/completions.ts
@@ -40,7 +40,7 @@ function parseOpenAIMessage(request: APIRequest) {
 
 function responseOpenAIMessage(content: string): APIResponse {
   const message: APIMessage = {
-    role: 'GPT-4',
+    role: 'bot',
     content,
   };
   return {
